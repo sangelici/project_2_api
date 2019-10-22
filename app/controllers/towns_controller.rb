b15e4b3004e0c624ff7bceefb0dc76adcdd2fb1b
sangelici <sangelici94@gmail.com>
2019-10-22 15:54:27 -0400

    Final commit

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -39,13 +39,14 @@ class TownsController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_town
-      @town = current_user.towns.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def town_params
-      params.require(:town).permit(:name, :town_fruit, :mayor, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_town
+    @town = current_user.towns.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def town_params
+    params.require(:town).permit(:name, :town_fruit, :mayor, :user_id)
+  end
 end
